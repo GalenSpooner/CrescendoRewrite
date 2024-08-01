c842eed371f97c2d5b87c8f30e252f56bca9bd33
GalenSpooner <gspooner@cusd.me>
2024-07-31 23:00:39 -0700

    Added human passing

--- Crescendo_Rewrite/src/main/java/frc/robot/RobotContainer.java
+++ Crescendo_Rewrite/src/main/java/frc/robot/RobotContainer.java
@@ -103,11 +103,7 @@ public class RobotContainer {
     leftJoystick.ThumbButton().onTrue(intake.setState(IntakeState.OUTTAKING));
     rightJoystick.DpadUp().onTrue(shooter.setState(PivotState.SPEAKER));
     rightJoystick.DpadLeft().or(rightJoystick.DpadRight()).onTrue(shooter.setState(PivotState.AMP));
-    if(modeChooser.getSelected() == "Outreach"){
-      rightJoystick.DpadDown().onTrue(shooter.setState(PivotState.HUMANPASS));
-    } else {
-      rightJoystick.DpadDown().onTrue(shooter.setState(PivotState.PASSING));
-    }
+    rightJoystick.DpadDown().onTrue((modeChooser.getSelected() == "Outreach") ? shooter.setState(PivotState.HUMANPASS) : shooter.setState(PivotState.PASSING));
     rightJoystick.DpadNeutral().onTrue(shooter.setState(PivotState.STOW));
     rightJoystick.trigger().onTrue(shooter.Score());
     
